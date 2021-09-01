ded702eece4c0cd5de7c2c2e4d506da8dc603836
David Gill <davegill@Davids-MBP.home>
2021-09-01 02:28:30 -0400

    change tile call

--- einops/_backends.py
+++ einops/_backends.py
@@ -391,7 +391,7 @@ class PaddleBackend(AbstractBackend):
         return self.paddle.stack(tensors)
 
     def tile(self, x, repeats):
-        return self.paddle.tile(x,repeats[0])
+        return self.paddle.tile(x,2)
 
     def add_axis(self, x, new_position):
         return self.paddle.unsqueeze(x, new_position)
