c8c241025e9b3833bdcabc334c8864bc8f46f934
David Gill <davegill@Davids-MBP.home>
2021-08-31 22:21:30 -0400

    paddle

--- einops/layers/paddle.py
+++ einops/layers/paddle.py
@@ -6,12 +6,12 @@ from ._weighted_einsum import WeightedEinsumMixin
 __author__ = 'Alex Rogozhnikov'
 
 
-class Rearrange(RearrangeMixin, paddle.layers.Layer):
+class Rearrange(RearrangeMixin, paddle.nn.Layer):
     def forward(self, input):
         return self._apply_recipe(input)
 
 
-class Reduce(ReduceMixin, paddle.layers.Layer):
+class Reduce(ReduceMixin, paddle.nn.Layer):
     def forward(self, input):
         return self._apply_recipe(input)
 
